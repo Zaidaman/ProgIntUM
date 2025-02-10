1aca588d79c30bf64e9926daa5a232d886251922
Zaidaman <cappella.filippo.it@gmail.com>
2025-02-10 20:55:41 +0100

    Fix

--- Progetto_UI/Services/Shared/Worker.cs
+++ Progetto_UI/Services/Shared/Worker.cs
@@ -11,7 +11,6 @@ namespace Progetto_UI.Services.Shared
     {
         [Key]
         public Guid Id { get; set; }
-
         public string Name { get; set; }
         public string Password { get; set; }
         public int action { get; set; }
