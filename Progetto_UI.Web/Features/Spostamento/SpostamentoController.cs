2c47e560ebb62292d89ead1006a2aa7d30027ef9
Zaidaman <cappella.filippo.it@gmail.com>
2025-06-07 15:19:18 +0200

    Update SpostamentoController.cs

--- Progetto_UI.Web/Features/Spostamento/SpostamentoController.cs
+++ Progetto_UI.Web/Features/Spostamento/SpostamentoController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Progetto_UI.Services.Shared;
 
 namespace Progetto_UI.Web.Features.Spostamento
 {
     public partial class SpostamentoController : Controller
     {
+        private readonly SharedService _sharedService;
+        public SpostamentoController(SharedService sharedService)
+        {
+            _sharedService = sharedService;
+        }
+
         [HttpGet]
         public virtual IActionResult Index()
         {
@@ -15,6 +24,27 @@ namespace Progetto_UI.Web.Features.Spostamento
         public virtual IActionResult Home()
         {
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> MovePieceToAnotherSpace(int pieceId, int SpaceId)
+        {
+            try
+            {
+                var cmd = new MovePieceToAnotherSpaceCommand
+                {
+                    PieceId = pieceId,
+                    NewSpaceId = SpaceId
+                };
+
+                await _sharedService.MovePieceToAnotherSpace(cmd);
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
         }
     }
 }
